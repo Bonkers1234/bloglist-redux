b595c7f4d366154f48ff758c236255e704e6d9b4
Bonkers <boroboro1@interia.pl>
2024-05-24 16:01:01 +0200

    Completed exercise 7.21

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,6 @@
 import { Link, useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { clearUser } from '../reducers/userReducer'
-import { notifyWith } from '../reducers/noticifationReducer'
 
 const Navbar = () => {
   const dispatch = useDispatch()
@@ -17,11 +16,24 @@ const Navbar = () => {
 
   return (
     <>
-      <div className='bg-info bg-gradient p-2'>
-        <Link to={'/'} className='btn btn-primary me-2' >blogs</Link>
-        <Link to={'/users'} className='btn btn-primary me-2' >users</Link>
-        <span>{user.name} logged in <button className='btn btn-light' onClick={logout}>logout</button></span>
-      </div>
+      <nav className='navbar navbar-expand-sm'>
+        <div className='container row bg-info bg-gradient position-relative p-2 w-100'>
+          <div className="col">
+            <button className='navbar-toggler' data-bs-toggle='collapse' data-bs-target='#nav'>
+              <span className='navbar-toggler-icon'></span>
+            </button>
+            <div className="collapse navbar-collapse" id='nav'>
+              <ul className='navbar-nav'>
+                <li className='nav-item my-1 my-sm-0'><Link to={'/'} className='btn btn-primary me-1' >blogs</Link></li>
+                <li className='nav-item my-1 my-sm-0'><Link to={'/users'} className='btn btn-primary me-1' >users</Link></li>
+              </ul>
+            </div>
+          </div>
+          <div className="col">
+            <span className='position-absolute top-0 end-0 m-2'>{user.name} logged in <button className='btn btn-light' onClick={logout}>logout</button></span>
+          </div>
+        </div>
+      </nav>
       <div>
         <h2>Blog app</h2>
       </div>
